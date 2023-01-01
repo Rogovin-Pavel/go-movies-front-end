bf4e50b7f14d764581e42913d6a59a1e2fb4e786
p.rogovin <p.rogovin@devinotele.com>
2023-01-01 12:31:00 +0400

    [GM-1] App, Home, Movies are added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
 import React from "react";
+import { Home } from "./components/Home";
 
 export const App = () => {
-  return <div>Go watch a movie</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go watch a movie</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!">
+            <span className="badge bg-success">Login</span>
+          </a>
+        </div>
+        <hr className="mb-3" />
+      </div>
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">
+                Home
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Movies
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Genres
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Add Movie
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Manage Catalogue
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                GraphQL
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10">
+          <Home />
+        </div>
+      </div>
+    </div>
+  );
 };
